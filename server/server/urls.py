dd5a437e0faa007f1d6060885d8fe49468a0e171
eliezerp3 <30153503+eliezerp3@users.noreply.github.com>
2019-04-02 05:38:47 -0700

    Moved registration to auth/registration so vuejs can find it

--- server/server/urls.py
+++ server/server/urls.py
@@ -5,7 +5,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     url(r'^api/', include('server.api.urls')),
     url(r'^auth/', include('rest_auth.urls')),
-    url(r'^registration/', include('rest_auth.registration.urls')),
+    url(r'^auth/registration/', include('rest_auth.registration.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name="index.html"), name='index'),
 ]
