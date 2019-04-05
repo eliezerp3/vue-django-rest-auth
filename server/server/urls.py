9a94f77e8b9bbb4ad194a8a382348af55cf75af7
eliezerp3 <30153503+eliezerp3@users.noreply.github.com>
2019-04-05 04:29:08 -0700

    Updated to "path" and defaults url to client for handling

--- server/server/urls.py
+++ server/server/urls.py
@@ -1,11 +1,12 @@
-from django.conf.urls import include, url
+from django.conf.urls import url
+from django.urls import path, include
 from django.contrib import admin
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    url(r'^api/', include('server.api.urls')),
-    url(r'^auth/', include('rest_auth.urls')),
-    url(r'^auth/registration/', include('rest_auth.registration.urls')),
-    url(r'^admin/', admin.site.urls),
-    url(r'^$', TemplateView.as_view(template_name="index.html"), name='index'),
+    path('api/', include('server.api.urls')),
+    path('auth/', include('rest_auth.urls')),
+    path('auth/registration/', include('rest_auth.registration.urls')),
+    path('admin/', admin.site.urls),
+    url(r'^.*$', TemplateView.as_view(template_name="index.html"), name='index'),
 ]
